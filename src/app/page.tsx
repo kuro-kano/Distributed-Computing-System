0d653aa6453f14633ad10971efcbabde4dec2441
tworamon <66070091@kmitl.ac.th>
2026-07-03 21:07:48 +0700

    add: render Board and ServerBanner on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import ServerBanner from "@/components/ServerBanner";
+import Board from "@/components/Board";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col">
-      <div>hello world</div>
+      <ServerBanner />
+      <div className="mx-auto w-full max-w-7xl flex-1 px-4 py-6 sm:px-6 lg:px-8">
+        <Board />
+      </div>
     </main>
-  )
+  );
 }
